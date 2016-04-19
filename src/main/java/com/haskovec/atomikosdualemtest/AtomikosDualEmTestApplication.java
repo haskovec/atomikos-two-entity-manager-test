0306c81d8cf722d645e3f8bc6f32f0e40adb2389
jhaskovec <Jeff.Haskovec@infor.com>
2016-04-19 16:49:16 -0500

    Check in initial spring boot project. Will wire in the databases later.

---
+++ src/main/java/com/haskovec/atomikosdualemtest/AtomikosDualEmTestApplication.java
@@ -0,0 +1,12 @@
+package com.haskovec.atomikosdualemtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomikosDualEmTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomikosDualEmTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/haskovec/atomikosdualemtest/AtomikosDualEmTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haskovec.atomikosdualemtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AtomikosDualEmTestApplication.class)
+public class AtomikosDualEmTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
